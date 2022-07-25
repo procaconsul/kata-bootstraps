0dbf52a0039242ea059198ffff4ef1abc733b611
Robert Chatley <rbc@imperial.ac.uk>
2022-07-25 11:56:14 +0100

    Removed Hiker test

--- ts/typescript-jest/src/Hiker.test.ts
+++ ts/typescript-jest/src/Hiker.test.ts
@@ -1,14 +1,4 @@
 import { Hiker } from './Hiker'
 
 describe('Hiker', () => {
-  it('answers 42', () => {
-    // Arrange
-    const hiker = new Hiker()
-
-    // Act
-    const answer = hiker.getAnswer()
-
-    // Assert
-    expect(answer).toBe(42)
-  })
 })
